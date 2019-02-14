fb8aa852bbf756b06c0ba672d4ef1f61ddb83b68
Prathyusha <prathyusham0001@gmail.com>
2019-02-13 22:33:04 -0800

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
+// Import the react and react dom libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// Create a react component
+const App = () =>{
+    return (
+        <div className="form-wrapper">
+            <div>
+                <label class="label" for="name">Enter Name: </label>
+                <input id="name" type="text"></input>
+                <button style={{backgroundColor:'blue',color:'white'}}>Submit</button>
+            </div>
+        </div>
+    )
+};
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Take the react component and show it on the screen
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+);
